302ac9221343820d1c8022089c41017c1328226e
maikytech <maikytech1307@gmail.com>
2022-03-15 10:14:28 -0500

    Add button

--- MemoryLeaks/ViewController.swift
+++ MemoryLeaks/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton()
+        button.setTitle("Tap Me", for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.center = view.center
+        view.addSubview(button)
     }
 
 
