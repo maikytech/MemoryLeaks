cb0693ea998a9c8cb2f2da2fe6cac1ed701d6162
maikytech <maikytech1307@gmail.com>
2022-03-15 09:08:11 -0500

    Start Project

---
+++ MemoryLeaks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeaks
+//
+//  Created by Maiqui Cedeño on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
